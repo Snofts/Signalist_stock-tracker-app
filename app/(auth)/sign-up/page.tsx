0b461e953bf96f736b53f8679f6d970c7a9451db
owolabisamuel <samuel@creativepluck.com>
2026-05-16 12:43:37 +0100

    Implement auth logic

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -6,8 +6,12 @@ import SelectField from "@/components/forms/SelectField";
 import { INVESTMENT_GOALS, PREFERRED_INDUSTRIES, RISK_TOLERANCE_OPTIONS } from "@/lib/constant";
 import { CountrySelectField } from "@/components/forms/CountrySelectField";
 import FooterLink from "@/components/forms/FooterLink";
+import { useRouter } from "next/navigation";
+import { signUpWithEmail } from "@/lib/actions/auth.actions";
+import { toast } from "sonner";
 
 const SignUp = () => {
+  const router = useRouter();
   const {
     register,
     handleSubmit,
@@ -27,9 +31,14 @@ const SignUp = () => {
   });
 const onSubmit = async (data: SignUpFormData) => {
   try {
-    console.log(data);
+    const result = await signUpWithEmail(data);
+    if(result.success) router.push('/')
+    
   } catch (error) {
     console.error(error);
+    toast.error('Sign up failed', {
+      description: error instanceof Error ? error.message : "Failed to create an account"
+    })
   }
 };
 
---
+++ lib/inngest/functions.ts
@@ -0,0 +1,50 @@
+import {inngest} from "@/lib/inngest/client"
+import { PERSONALIZED_WELCOME_EMAIL_PROMPT } from './prompt';
+import { StepBack } from "lucide-react";
+import { sendWelcomeEmial } from "../nodemailer";
+
+export const sendSignUpEmail= inngest.createFunction(
+    {id: "sign-up-email"},
+    {event: "app/user.created"},
+    async ({event, step}) => {
+        const userProfile = `
+            -Country: ${event.data.country}
+            - Investment goals: ${event.data.investmentGoals}
+            - Risk tolerance: ${event.data.risksTolerance}
+            - Preferred industry: ${event.data.preferredIndustry}
+            - Investment goals: ${event.data.investmentGoals}
+        `
+
+        const prompt = PERSONALIZED_WELCOME_EMAIL_PROMPT.replace("{{user_profile}}", userProfile) 
+
+        const response = await step.ai.infer("generate-welcome-intro", {
+            model: step.ai.models.gemini({model: "gemini-2.0-flash"}),
+                body: {
+                    contents: [
+                        {
+                            role: "user",
+                            parts: [
+                                {text: prompt}
+                            ]
+                        }
+                    ]
+                }
+            
+        })
+
+        await step.run("send-welcome-email", async () => {
+            const part = response.candidates?.[0]?.content?.parts?.[0];
+            const introText = (part && "text" in part ? part.text : null) || 'Thanks for joining signalist. You now have the tool to track market and make smarter moves.'
+
+            const {data:{email, name} } = event;
+
+            return await sendWelcomeEmial({email, name, intro: introText})
+        })
+
+        return {
+            success: true,
+            message: "Welcome email sent successfully"
+        }
+    }
+
+)
